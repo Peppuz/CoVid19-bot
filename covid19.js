7b9f873d70ac0cb5acb2bed5bdf1f8a717cc2a7a
Peppuz <peppuzvitale@gmail.com>
2020-02-26 16:05:35 +0100

    Minor Adjustments (markdown text) and Fixed saved on file

--- covid19.js
+++ covid19.js
@@ -6,26 +6,17 @@ const fs = require("fs");
  * This Telegram bot is intended to check the Italian situation 
  * on CoVid-19 reporting automatically every hour (or when requested)
  * 
- *  # To install $: 
- * [ ] git clone https://github.com/peppuz/covid19-bot.git
- * [ ] npm install --save node-telegram-bot-api puppeteer 
- * [ ] node covid19.js
- * 
  * License: MIT
  * Author: Peppuz - peppuzvitale@gmail.com
  */
 
-
 var data = require('./covid19.json') // file Open a json 
 const sito = 'http://www.salute.gov.it/nuovocoronavirus'
-const token = '911573383:AAEgF34nKY7-OYtQ2eeoymfI4CGSIM-iL6w';
+const token = ''; // ADD HERE YOUR  TOKEN
 const bot = new TelegramBot(token, { polling: true });
 
 // Bot Event Handler - Reply to any message incoming
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, data);
-});
+bot.on('message', (msg) => bot.sendMessage(msg.chat.id, data, { parse_mode: 'markdown' }))
 
 // Puppeteer scraper
 async function BrowserCall() {
@@ -35,14 +26,13 @@ async function BrowserCall() {
   await page.goto(sito)
   await page.waitFor(1000)
   const initial_text = await page.$eval("#datiItalia", e => e.textContent)
-  console.log("initial_text", initial_text.trim());
 
   await browser.close()
   console.log('[ ] CLOSING BROWSER ');
 
   const proc = initial_text.split('\n')
 
-  let text = `Ultimo Aggiornamento da / Last Update from : \n${sito}\n\n`
+  let text = `Ultimo Aggiornamento da :\n_${sito}_\n\n`
   for (let a = 0; a < proc.length; a++) {
     const element = proc[a];
     if (element === "" || element === " ")
@@ -51,25 +41,28 @@ async function BrowserCall() {
     switch (a) {
       case 4:
         let c = b.split(' ')
-        for (let a = 0; a < 5; a++) c.pop(0) // TODO check index  
+        // for (let a = 0; a < 3; a++)
+        // c.pop(0)
+        // c.pop(0)
         text += c.join(' ') + '\n\n' // Add Data e ora last update
-        console.log(text);
         break;
       case 15: //positivi
-        text += `Positivi al Corona Virus in Italia: ${b}\n`
+        text += `*Positivi*: ${b}\n`
         break;
       case 23: //deceduti
-        text += `Deceduti: ${b}\n`
+        text += `*Deceduti*: ${b}\n`
         break;
       case 31: //Guariti
-        text += `Guariti (dimessi): ${b}\n`
+        text += `*Guariti* (dimessi): ${b}\n`
         break;
       default:
         break;
     }
   }
+  console.log("[ ] FULL Text scraped");
+
   // save for the masses without using a database
-  fs.writeFile("data.json", text, (err) => { if (err) console.log(err); console.log("Successfully Written to File."); });
+  fs.writeFile("./covid19.json", text, (err) => { if (err) console.log(err); console.log("Successfully Written to File."); });
   data = text
 }
 
